7166df32871607707c0313720ed56201953085e6
juan <juanmroude@gmail.com>
2024-06-25 09:14:28 -0300

    prueba inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    include 'components/head.php';
+    ?>
+</head>
+<body>
+    <header>
+    <?php include 'components/nav.php';  ?>
+    </header>
+
+
+<footer>
+<?php include 'components/footer.php';  ?>
+</footer>
+
+</body>
+</html>
