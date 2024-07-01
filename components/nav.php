b789be16adc3dc6145a5a81dd67020c12ede9b69
juan2-git <juanmroude@yahoo.com.ar>
2024-07-01 10:24:36 -0300

    arreglo nav

--- components/nav.php
+++ components/nav.php
@@ -10,13 +10,10 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Productos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Contacto</a>
         </li>
       </ul>
     </div>
